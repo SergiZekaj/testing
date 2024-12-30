14b0f650c197a7101cf24acb26bcd3953a7adaf9
Sergi-crypto <130180894+Sergi-crypto@users.noreply.github.com>
2024-12-30 17:19:17 +0100

    first commit

---
+++ Java Project/StartDoctor.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+import Frames.*;
+
+public class StartDoctor {
+    public static void main(String[] args){
+        Login lg =  new Login();
+        lg.setVisible(true);
+	/*	Menu me =  new Menu();
+		me.setVisible(true);*/
+    }
+}
